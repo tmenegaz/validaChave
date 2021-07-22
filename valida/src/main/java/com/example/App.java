ceb855dbe3670b8b56fbe4d98fd62a4ca108f1a6
tmenegaz <tmenegaz@gmail.com>
2021-07-22 08:48:59 -0300

    nova entrada para compor id da nota xml e gerar o cDV

--- valida/src/main/java/com/example/App.java
+++ valida/src/main/java/com/example/App.java
@@ -6,7 +6,8 @@ public class App
     public static void main( String[] args )
     {
         Entrada entrada = new Entrada();
-        entrada.validaChave();
-        System.out.println("Dígito verificador:\t"+entrada.cdv()+ " OK");
+        // entrada.digitaCampos();
+        entrada.criaChave();
+        
     }
 }
--- valida/src/main/java/com/example/Entrada.java
+++ valida/src/main/java/com/example/Entrada.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +11,7 @@ public class Entrada {
     private List<Integer> chave;
     private List<Integer> peso;
     private Integer cDV;
+    private String comp;
     private StringBuilder sb;
 
     Scanner sc;
@@ -30,65 +32,92 @@ public class Entrada {
         sb = new StringBuilder();
         sc = new Scanner(System.in);
     }
-
+    
     public void digitaCampos(){
         util();
         
-        System.out.print("Digite o UF: ");
-        String uf = sc.nextLine();
-        // String uf = "52";
+        // System.out.print("Digite o UF: ");
+        // String uf = sc.nextLine();
+        String uf = "43";
         
-        System.out.print("Digite o aamm: ");
-        String aamm = sc.nextLine();
-        // String aamm = "0604";
+        // System.out.print("Digite o aamm: ");
+        // String aamm = sc.nextLine();
+        String aamm = "2107";
         
-        System.out.print("Digite o cnpj_cpf: ");
-        String cnpj_cpf = sc.nextLine();
-        // String cnpj_cpf = "33009911002506";
+        // System.out.print("Digite o cnpj_cpf: ");
+        // String cnpj_cpf = sc.nextLine();
+        String cnpj_cpf = "88709621000190";
         
-        System.out.print("Digite o mod: ");
-        String mod = sc.nextLine();
-        // String mod = "55";
+        // System.out.print("Digite o mod: ");
+        // String mod = sc.nextLine();
+        String mod = "55";
         
-        System.out.print("Digite o serie: ");
-        String serie = sc.nextLine();
-        // String serie = "012";
+        // System.out.print("Digite o serie com 3 dígitos (manter zeros a esquerda): ");
+        // String serie = sc.nextLine();
+        String serie = "002";
         
-        System.out.print("Digite o nnf: ");
-        String nnf = sc.nextLine();
-        // String nnf = "000000780";
+        // System.out.print("Digite o nnf  com 9 dígitos (manter zeros a esquerda): ");
+        // String nnf = sc.nextLine();
+        String nnf = "000258959";
         
-        System.out.print("Digite o tpemis: ");
-        String tpemis = sc.nextLine();
-        // String tpemis = "0";
+        // System.out.print("Digite o tpemis: ");
+        // String tpemis = sc.nextLine();
+        String tpemis = "1";
         
-        System.out.print("Digite o cnf: ");
-        String cnf = sc.nextLine();
-        // String cnf = "26730161";
+        // System.out.print("Digite o cnf: ");
+        // String cnf = sc.nextLine();
+        String cnf = "71802232";
         
         sb.append(uf+aamm+cnpj_cpf+mod+serie+nnf+tpemis+cnf);
         for (int i = 0; i < sb.toString().length(); i++){
             chave.add(Integer.parseInt(sb.substring(i,(i+1))));
         }
-        sc.close();
-    }
 
-    public void validaChave(){
-        util();
-        System.out.print("Digite a chave com 44 caracteres numéricos:\t");
-        sb.append(sc.nextLine());
-        // sb.append("41180678393592000146558900000006041028190697");
-        // sb.append("52060433009911002506550120000007800267301615");
-        cDV = Integer.parseInt(sb.toString().substring(43, 44));
-
-        sb.delete(43, 44);
-        for (int i = 0; i < sb.toString().length(); i++){
-            chave.add(Integer.parseInt(sb.substring(i,(i+1))));
-        }
+        comp = sb.toString();
+        System.out.println("Composição -->> "+comp);
+        
         sc.close();
     }
     
-    public Integer cdv(){
+    public void criaChave(){
+        util();
+        System.out.print("Digite a chave com 43 caracteres numéricos - (chave sem o dígito):\t");
+        sb.append(sc.nextLine());
+        // sb.append("4321078870962100019055002000258959171802232");
+        // sb.append("41180678393592000146558900000006041028190697");
+        // sb.append("52060433009911002506550120000007800267301615");
+        String uf = sb.toString().substring(0, 2);
+        String aamm = sb.toString().substring(2, 6);
+        String cnpj_cpf = sb.toString().substring(6, 20);
+        String mod = sb.toString().substring(20, 22);
+        String serie = sb.toString().substring(22, 25);
+        String nnf = sb.toString().substring(25, 34);
+        String tpemis = sb.toString().substring(34, 35);
+        String cnf = sb.toString().substring(35, 43);
+        
+        for (int i = 0; i < sb.toString().length(); i++){
+            chave.add(Integer.parseInt(sb.substring(i,(i+1))));
+        }
+
+        cdv();
+        System.out.println(
+            "\n<cUF>"+uf+"</cUF>"+
+            "\n<cNF>"+cnf+"</cNF>"+
+            "\n<mod>"+mod+"</mod>"+
+            "\n<serie>"+serie+"</serie>"+
+            "\n<nNF>"+nnf+"</nNF>"+
+            "\n<dEmi>"+aamm.toString().substring(0, 2)+"-"+aamm.toString().substring(2, 4)+"</dEmi>"+
+            "\n<tpEmis>"+tpemis+"</tpEmis>"+
+            "\n<cDV>"+cDV+"</cDV>"+
+            "\n<CNPJ>"+cnpj_cpf+"</CNPJ>");
+        
+        comp = sb.toString();
+        System.out.println("Composição -->> "+comp);
+        System.out.println("Chave com cDV:\t"+comp+cDV);
+        sc.close();
+    }
+    
+    public void cdv(){
 
         int i = 0;
         int soma = 0;
@@ -104,14 +133,13 @@ public class Entrada {
         if (cDV == null){
             cDV = (11-(soma%11));
         } else if(cDV == (11-(soma%11))){
-            return cDV;
+            cDV = cDV;
         }
         
         if(cDV <= 1){
             cDV = 0;
-            return cDV;
         } else {
-            return cDV;
+            cDV = cDV;
         }
     }
 }
